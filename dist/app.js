9896103ed24b73433ff70d738c5376dd6988396b
Dell E5580 <malunka221998@gmail.com>
2023-04-04 13:53:58 +0300

    sockets

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const socket_io_1 = require("socket.io");
+const http = require('http');
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
@@ -11,9 +13,34 @@ const configs_1 = require("./configs");
 const routers_2 = require("./routers");
 const crons_1 = require("./crons");
 const app = (0, express_1.default)();
+const server = http.createServer(app);
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, express_fileupload_1.default)());
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+io.on('connection', async (socket) => {
+    const sockets = await io.fetchSockets();
+    sockets.forEach((socket) => {
+        console.log(socket.id);
+    });
+    socket.on('message:send', (text) => {
+        io.emit('message:get', text);
+    });
+    socket.on('join:room', ({ roomId }) => {
+        socket.join(roomId);
+        socket
+            .to(roomId)
+            .emit('user:joined', { socketId: socket.id, action: 'Joined' });
+    });
+    socket.on('leave:room', ({ roomId }) => {
+        socket.leave(roomId);
+        socket.to(roomId).emit('user:leave', { socketId: socket.id, action: 'Leave' });
+    });
+});
 app.use("/users", routers_1.userRouter);
 app.use("/auth", routers_2.authRouter);
 app.use((err, req, res, next) => {
@@ -23,7 +50,7 @@ app.use((err, req, res, next) => {
         status,
     });
 });
-app.listen(configs_1.configs.PORT, async () => {
+server.listen(configs_1.configs.PORT, async () => {
     await mongoose_1.default.connect(configs_1.configs.DB_URL);
     (0, crons_1.cronRunner)();
     console.log(`start${configs_1.configs.PORT}`);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
+// import {locale} from "dayjs";
+
+import {Server, Socket} from "socket.io";
+
+const http = require('http')
 
 import express, {NextFunction, Request, Response} from "express";
 import mongoose from "mongoose";
@@ -11,9 +16,61 @@ import {cronRunner} from "./crons";
 
 
 const app = express();
+const server = http.createServer(app)
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUploader());
+
+const io = new Server(server, {
+  cors: {
+    origin:"*",
+  },
+  });
+
+io.on('connection', async (socket:Socket) => {
+
+  // console.log(socket.handshake.auth.token)
+const sockets = await io.fetchSockets();
+
+sockets.forEach((socket)=>{
+  console.log(socket.id);
+
+})
+
+  /** Send to particular client */
+  // socket.emit('message', {message: 'hello'})
+
+  /** Send to all client */
+  // io.emit('user:connected',{message: 'user connected'})
+
+  /** Send to all client without sender */
+  // socket.broadcast.emit('user:connected',{message: 'user connected'})
+
+  socket.on('message:send',(text)=>{
+  io.emit('message:get', text)
+  })
+
+  // socket.on('disconnect', ()=>{
+  //   console.log(`${socket.id} disconnect`)
+  // })
+
+  socket.on('join:room', ({roomId})=>{
+    socket.join(roomId)
+
+    socket
+        .to(roomId)
+        .emit('user:joined', {socketId:socket.id, action:'Joined'})
+  })
+    socket.on('leave:room', ({roomId})=>{
+      socket.leave(roomId);
+      socket.to(roomId).emit('user:leave',{socketId:socket.id, action:'Leave'} )
+    })
+})
+
+
+
+
 
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
@@ -31,7 +88,7 @@ app.use((err:IError, req:Request, res:Response, next: NextFunction)=>{
 })
 
 
-app.listen(configs.PORT, async () => {
+server.listen(configs.PORT, async () => {
   await mongoose.connect(configs.DB_URL);
   cronRunner();
   console.log(`start${configs.PORT}`);
